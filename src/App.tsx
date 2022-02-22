62f4f44e27a68ecc9cca048eb5551f4e57f32ecf
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-02-22 16:41:20 -0600

    Added CounterManagment component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import CounterManagement from './components/CounterManagement';
 
 function App() {
   return (
     <>
       <h1>My App</h1>
+      <CounterManagement ownerName='idk' />
     </>
   );
 }
