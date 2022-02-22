4da79ff9ce8b5e8ec4a42f77c7da82d9f6c408aa
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-02-22 16:15:21 -0600

    Created Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { Button } from './components/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>App</h1>
+      <Button type='primary'/>
+      <Button />
+      <Button type='primary'>Primary</Button>
+      <Button>Default</Button>
+    </>
   );
 }
 
