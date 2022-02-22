ccad7649f6bb056adc45f67cc2522ea3736a3b4e
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-02-22 16:32:42 -0600

    Erased Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import { Button } from './components/Button';
 
 function App() {
   return (
     <>
-      <h1>App</h1>
-      <Button type='primary'/>
-      <Button />
-      <Button type='primary'>Primary</Button>
-      <Button>Default</Button>
+      <h1>My App</h1>
     </>
   );
 }
