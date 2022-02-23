13f2a202c0d6b1d6eba9d597624dd088c143add5
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-02-23 11:55:45 -0600

    Added logic to fetch from API

--- src/components/CounterManagement/index.tsx
+++ src/components/CounterManagement/index.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 import { CounterManagementState, CounterManagementProps } from './interface';
 
@@ -6,7 +7,8 @@ export default class CounterManagement extends React.Component<CounterManagement
     super(props);
 
     this.state = {
-      counter: 0
+      counter: 0,
+      users: []
     }
   }
 
@@ -18,9 +20,29 @@ export default class CounterManagement extends React.Component<CounterManagement
     this.setState({ counter: this.state.counter - 1});
   }
 
+  clickWindow = () => {
+    this.setState({ counter: this.state.counter + 1});
+  }
+
+  componentDidMount() {
+    axios.get('https://reqres.in/api/users?page=2')
+      .then(res => {
+        const data = res.data;
+
+        const users = data.data.map((userData: any) => userData.first_name);
+
+        this.setState({ users });
+      })
+    window.addEventListener('click', this.clickWindow);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.clickWindow);
+  }
+
   render() {
     const { ownerName } = this.props;
-    const { counter } = this.state;
+    const { counter, users } = this.state;
     return (
       <div>
         <h1> Counter Management </h1>
@@ -28,6 +50,9 @@ export default class CounterManagement extends React.Component<CounterManagement
         <h3> Counter: {counter} </h3>
         <button onClick={this.handleAddClick}>Add</button>
         <button onClick={this.handleMinusClick}>Minus</button>
+        <ul>
+          {users.map(user => <li>{user}</li>)}
+        </ul>
       </div>
     )
   }
--- src/components/CounterManagement/interface.ts
+++ src/components/CounterManagement/interface.ts
@@ -3,5 +3,6 @@ export interface CounterManagementProps {
 }
 
 export interface CounterManagementState {
-  counter: number
+  counter: number;
+  users: string[];
 }
