fac775629b8d014a647eb83f2339824e5f88777f
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-03-04 12:01:23 -0600

    Installed express

--- src/reducers/NotesAction/index.ts
+++ src/reducers/NotesAction/index.ts
@@ -1,36 +1,36 @@
 import { Reducer } from "redux";
 
 export type Note = {
-    title: string;
-    body: string;
-    lastModified: Date;
+  title: string;
+  body: string;
+  lastModified: Date;
 }
 
 export interface NotesAction {
-    type: string;
-    notes: Note[];
-    index?: number;
+  type: string;
+  notes: Note[];
+  index?: number;
 }
 
 export const notesReducer: Reducer<Note[], NotesAction> = (state = [], action) => {
-    const { notes , index: indexNote, type } = action;
-    
-    switch(type) {
-        case "ADD_NOTES":
-            return [ ...state, ...action.notes ];
-        case "MODIFY_NOTES":
-            const modifiedNote = notes[0];
-            return state.map(((note, index) => {
-                if (index == indexNote) {
-                    note.body = modifiedNote.body;
-                    note.title = modifiedNote.title;
-                    note.lastModified = modifiedNote.lastModified;
-                }
-                return note;
-            }));
-        case "DELETE_NOTE":
-            return state.filter((note, index) => index !== indexNote);
-        default:
-            return state;
-    }
+  const { notes , index: indexNote, type } = action;
+  
+  switch(type) {
+    case "ADD_NOTES":
+      return [ ...state, ...action.notes ];
+    case "MODIFY_NOTES":
+      const modifiedNote = notes[0];
+      return state.map(((note, index) => {
+        if (index == indexNote) {
+          note.body = modifiedNote.body;
+          note.title = modifiedNote.title;
+          note.lastModified = modifiedNote.lastModified;
+        }
+        return note;
+      }));
+    case "DELETE_NOTE":
+      return state.filter((note, index) => index !== indexNote);
+    default:
+      return state;
+  }
 }
